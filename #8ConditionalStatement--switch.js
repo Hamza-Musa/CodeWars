7a6c4151a739620e7bd6e1fda010770824510838
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2023-09-02 19:06:57 +0100

    daily cw challenge

--- #8ConditionalStatement--switch.js
+++ #8ConditionalStatement--switch.js
@@ -66,3 +66,36 @@ Tip: Using default for most of the cases can reduce your work.
 
 When you have finished, click "Test" to test your code against initial tests and "Attempt" to test your code against all tests. If you pass in all tests you can click "Submit" to submit your code.
 */
+
+//Answer
+//P.R.E.P Parameters, Returns, Examples, Pseudo Code
+
+//P: string
+//R: days
+//E: howManydays(3) = returns 31
+//P: feb = 28 days
+
+function howManydays(month) {
+  var days;
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      days = 31;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      days = 30;
+      break;
+    case 2:
+      days = 28;
+      break;
+  }
+  return days;
+}
