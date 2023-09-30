77deb91c1eba0188cdfd59ae79dc799ace46d478
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2023-09-30 17:12:34 +0100

    CW prep

--- Plural.js
+++ Plural.js
@@ -9,3 +9,17 @@ All values will be positive integers or floats, or zero.
 
 
 */
+
+//P parameters - num - +, whole number
+//R - if its non plural return false if it is return true
+//E
+
+function plural(n) {}
+
+//test cases
+console.log(plural(0)); // return true
+console.log(plural(0.5)); // return true
+console.log(plural(1)); // return false
+console.log(plural(100)); // return true
+
+//P
