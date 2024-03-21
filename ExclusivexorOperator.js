166acd208e84e85ea0e56d346dbaa4c0b14816eb
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2024-03-21 11:13:38 +0000

    Daily cw

--- ExclusivexorOperator.js
+++ ExclusivexorOperator.js
@@ -22,3 +22,8 @@ console.log(xor(false, false)); // false
 console.log(xor(true, false)); // true
 console.log(xor(false, true)); // true
 console.log(xor(true, true)); //false
+
+//Best Practice:
+// function xor(a, b) {
+//   return a != b;
+// }
