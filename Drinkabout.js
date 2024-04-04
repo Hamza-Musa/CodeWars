86c136970586de3d34db013ede2f983919048873
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2024-04-04 18:53:16 +0100

    Daily Cw challenge answer

--- Drinkabout.js
+++ Drinkabout.js
@@ -23,15 +23,25 @@ Examples: (Input --> Output)
 
 */
 
+//Answer
+
 //P number -age
 //R - conditional - string
 //E '13 - drink coke'
 //P
 
-// kids drink toddy
-
-//teens drink coke
-
-//young adults drink beer
-
-//adults drink whisky
+function peopleWithAgeDrink(old) {
+  // kids drink toddy
+  if (old < 14) {
+    return "drink toddy";
+    //teens drink coke
+  } else if (old < 18) {
+    return "drink coke";
+    //young adults drink beer
+  } else if (old < 21) {
+    return "drink beer";
+    //adults drink whisky
+  } else {
+    return "drink whisky";
+  }
+}
