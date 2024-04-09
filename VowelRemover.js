5f7e914c0c4efa39e122527ffe2a5c1bf7c17338
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2024-04-09 17:23:39 +0100

    Daily Cw challenge prep

--- VowelRemover.js
+++ VowelRemover.js
@@ -11,3 +11,27 @@ y is not considered a vowel for this kata
 
 
 */
+
+//P (parameters)
+//R (return)
+//E (example)
+//P (seudo)
+
+// .split("") - to make word into array of letters
+
+//if case - check if word has vowels
+
+//return .pop - aka remove vowles and return back to the word
+//join("")
+// firrst need to figure out how to remove vowels
+// then it being only lowercase
+
+// make - string into an array of letters
+// if any of the strings have vowel letters - a, e, i, o, u
+// .pop it and return the answer
+
+//Answer
+
+function shortcut(string) {
+  const letters = string.split("");
+}
