bd80c950e6d70ef4d7307c6e4173b4d9aa78a07f
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2024-04-11 18:32:39 +0100

    Daily Cw challenge answer

--- Filteroutthegeese.js
+++ Filteroutthegeese.js
@@ -4,7 +4,15 @@ Your function would return the following array:
 ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
 The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
 
-
-
-
 */
+
+//P(arameters) array of strings
+//R(eturn) return array that does include any of geeses from geese array
+//E(xample) For example, if this array were passed as an argument:
+//["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+// function should return the following array:
+// ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+
+//P(seudo)
+
+// .filter method, and .includes method, make false true
