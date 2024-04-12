5cdd40eafe97575a8ff012f67ca348fe4fddc8a7
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2024-04-12 18:14:41 +0100

    Daily Cw challenge answer

---
+++ Filteroutthegeese.js
@@ -0,0 +1,10 @@
+/* Instructions:
+Your function would return the following array:
+
+["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+
+
+
+
+*/
