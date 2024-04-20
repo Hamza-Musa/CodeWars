a3dc4ef5b5253442778aaaf6cb39231e00e33885
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2024-04-20 12:32:58 +0100

    Daily cw

--- TrainingJS#7.js
+++ TrainingJS#7.js
@@ -16,3 +16,8 @@ function saleHotdogs(n) {
   else if (n >= 5 && n < 10) return n * 95;
   else return n * 90;
 }
+
+//Best Practice
+function saleHotdogs(n) {
+  return n * (n < 5 ? 100 : n < 10 ? 95 : 90);
+}
