76550e3e9eba432a1798a4d8765124143bf28ba1
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2024-04-25 12:10:38 +0100

    Daily cw challenge

--- ExclusivexorOperator.js
+++ ExclusivexorOperator.js
@@ -11,5 +11,14 @@ Task
 Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
 
 
-
 */
+
+//Answer
+function xor(a, b) {
+  return (a || b) && !(a && b);
+}
+
+console.log(xor(false, false)); // false
+console.log(xor(true, false)); // true
+console.log(xor(false, true)); // true
+console.log(xor(true, true)); //false
