c817458cb6b36db3cfa19df3a27cac1c69d485e0
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2024-05-09 11:54:34 +0100

    daily cw

---
+++ GetthemeanofanArray.js
@@ -0,0 +1,20 @@
+/*Instructions:
+It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+
+*/
+
+//P - arr, num
+//R average - add all numbers divide by amound
+//E -
+
+function getAverage(marks) {}
+
+console.log(getAverage([2, 2, 2, 2])); // should equal
+console.log(getAverage([1, 2, 3, 4, 5]));
+console.log(getAverage([1, 1, 1, 1, 1, 1, 1, 2]));
+
+//P
