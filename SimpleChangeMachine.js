ec71c7be47a9b237367efa606c8da34d6c71f8ab
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2024-05-17 13:09:49 +0100

    cw prep

--- SimpleChangeMachine.js
+++ SimpleChangeMachine.js
@@ -8,3 +8,24 @@
 // This change machine is programmed to accept and distribute strings rather than numbers. The input will be a string containing the coin or note to be processed, and the change should be returned as one string with the coin names separated by single spaces with no commas. The values of the string should be in descending order. For example, an input of "50p" should yield the exact string "20p 20p 10p".
 
 //Answer
+
+//P - string of the currency - "50p"
+//R - return the change in either 20p or 10p coins
+//E -
+//P
+
+//conditionals?
+//while loop - while moneyIn = 1 return this
+
+function changeMe(moneyIn) {
+  // Write your function here
+  const validChange = ["£5, £2, £1, 50p, 20p"];
+  if (!validChange.includes(moneyIn)) {
+    console.log("Invalid money denomination.");
+  } else {
+    console.log("Valid money denomination.");
+  }
+}
+
+console.log("£1"); // "20p" "20p" "20p" "20p" "20p"
+console.log("50p"); // "20p" "20p" "10p"
