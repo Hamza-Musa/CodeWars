88472a5ec744717077107c6b41eefc2d7c0411b8
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2024-06-08 12:45:45 +0100

    daily cw

--- CSVRepresentationOfArray.js
+++ CSVRepresentationOfArray.js
@@ -20,6 +20,24 @@ More details here: https://en.wikipedia.org/wiki/Comma-separated_values
 
 Note: you shouldn't escape the \n, it should work as a new line.
 
-
-
 */
+
+//answer
+
+//Parameters - array
+//Return
+//Example
+//Psuedocode
+
+function toCsvText(array) {
+  return array.join("\n");
+}
+
+console.log(
+  toCsvText(
+    [0, 1, 2, 3, 45],
+    [10, 11, 12, 13, 14],
+    [20, 21, 22, 23, 24],
+    [30, 31, 32, 33, 34]
+  )
+);
