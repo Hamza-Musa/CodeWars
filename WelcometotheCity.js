535d2dcb790cfc77df8a8e6039df028d328978d2
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2024-06-19 00:09:47 +0100

    cw

--- WelcometotheCity.js
+++ WelcometotheCity.js
@@ -8,3 +8,11 @@ This example will return the string Hello, John Smith! Welcome to Phoenix, Arizo
 
 
 */
+
+//Answer
+function sayHello(name, city, state) {
+  return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
+}
+
+sayHello(["John", "Smith"], "Phoenix", "Arizona");
+// "Hello, John Smith! Welcome to Phoenix, Arizona!"
