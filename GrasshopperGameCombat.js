b735e9e02837a0fdd2ab7300e028a41f98564c60
Hamza Musa <hmusa1202@gmail.com>
2024-06-26 22:45:02 +0100

    Create GrasshopperGameCombat.js, task

---
+++ GrasshopperGameCombat.js
@@ -0,0 +1,8 @@
+/*Task:
+Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+
+
+*/
+
+//Answer
