49873a93c7c40cad4a812981c6442dff17b3872b
Hamza Musa <hmusa1202@gmail.com>
2024-07-03 18:55:39 +0100

    daily cw

--- GravityFlip.js
+++ GravityFlip.js
@@ -23,3 +23,14 @@ Examples (input -> output:
 * 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
 
 */
+
+//Answer
+
+//P two arguments - one for the direction other for the array
+//R if R asc, if L dsc
+//E
+//P
+//use .sort method to sort array into sc if 'r' : 'l'
+
+console.log(flip("R", [3, 2, 1, 2])); // [1, 2, 2, 3]);
+console.log(flip("L", [1, 4, 5, 3, 5])); // [5, 5, 4, 3, 1]);
