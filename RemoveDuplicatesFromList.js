ed79baf6ed37d99d8362d02e760da1e8c49ce8e6
Hamza Musa <hmusa1202@gmail.com>
2024-07-06 14:31:44 +0100

    daily cw

---
+++ RemoveDuplicatesFromList.js
@@ -0,0 +1,24 @@
+/* Instructions:
+Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+Examples:
+
+Input -> Output
+[1, 1, 2] -> [1, 2]
+[1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+
+*/
+
+//P arr, non negative numbers +,
+//R return arr without duplicates
+//E
+//P
+
+//Answer
+
+console.log(distinct([1])); // [1]
+console.log(distinct([1, 2])); //[1,2];
+console.log(distinct([1, 1, 2])); //[1,2]
