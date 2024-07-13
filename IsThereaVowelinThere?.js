94de13c2b55401e814c490d24a0492463ff05c38
Hamza Musa <hmusa1202@gmail.com>
2024-07-14 00:03:52 +0100

    daily codewar

--- IsThereaVowelinThere?.js
+++ IsThereaVowelinThere?.js
@@ -10,7 +10,30 @@ Return the resulting array.
 
 */
 
-//P
-//R
+//P - arr - num
+//R - replace ascii value with the actual letter
 //E
 //P
+//loop through array, find ascii values relates to vowels
+//replace
+
+//answer:
+function isVow(a) {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === 97) {
+      a[i] = "a";
+    } else if (a[i] === 101) {
+      a[i] = "e";
+    } else if (a[i] === 105) {
+      a[i] = "i";
+    } else if (a[i] === 111) {
+      a[i] = "o";
+    } else if (a[i] === 117) {
+      a[i] = "u";
+    }
+  }
+  return a;
+}
+
+//test
+console.log(isVow([95, 96, 97, 98, 102, 105])); // 95, 96, "a", 98, 102, "i"
