e01b83a3132cf3ebae866edc56e3f640843b60b1
Hamza Musa <hmusa1202@gmail.com>
2024-07-22 22:15:43 +0100

    daily cw

---
+++ RemoveFirstandLastCharpt2.js
@@ -0,0 +1,42 @@
+/* 
+Task:
+This is a spin off of my first kata.
+
+You are given a string containing a sequence of character sequences separated by commas.
+
+Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+Examples
+"1,2,3"      =>  "2"
+"1,2,3,4"    =>  "2 3"
+"1,2,3,4,5"  =>  "2 3 4"
+
+""     =>  NULL
+"1"    =>  NULL
+"1,2"  =>  NULL
+
+*/
+
+//answer
+
+// string - char seperated with commas
+// return arr into a string without commas, also if 2< return null
+//
+
+function removeChar(str) {
+    // null cases
+    if(str.length < 3){
+        return null
+    }
+}
+
+console.log(removeChar("")); // NULL
+console.log(removeChar("1")); // NULL
+console.log(removeChar("1,2")); // NULL
+
+console.log(removeChar("1,2,3")); // "2"
+console.log(removeChar("1,2,3,4")); // "2 3"
+console.log(removeChar("1,2,3,4,5")); // "2 3 4"
+
