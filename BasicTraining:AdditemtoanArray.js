7706f6392ca430cef10c7a53c18cfee5a48b1b10
Hamza Musa <hmusa1202@gmail.com>
2024-08-04 11:41:21 +0100

    daily codwar

---
+++ BasicTraining:AdditemtoanArray.js
@@ -0,0 +1,15 @@
+/*
+Task:
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+
+*/
+
+//Answer
+// add the value "codewars" to the already defined websites array
+websites.push("codewars");
