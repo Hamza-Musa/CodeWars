263e7c172bbd41fea61b66c1c6045feee685539e
Hamza Musa <hmusa1202@gmail.com>
2024-08-13 13:32:40 +0100

    daily cw

---
+++ PushaHash/objIntoArr.js
@@ -0,0 +1,9 @@
+/*Task:
+You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+
+
+*/
+
+//answer
+items = [];
+items.push({ a: "b", c: "d" });
