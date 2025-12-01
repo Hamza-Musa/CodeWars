bee3942a760ead64362477bb4643c2a39b7c5752
Hamza Musa <hmusa1202@gmail.com>
2025-12-01 00:44:11 +0000

    antoher 1 dj khaled

---
+++ CountOddNumbersbelown.js
@@ -0,0 +1,16 @@
+/* 
+Given a number n, return the number of positive odd numbers below n, EASY!
+
+Examples (Input -> Output)
+7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+
+*/
+
+//answer 
+function oddCount(n){
+  return n % 2 === 0;
+}
+
+console.log(oddCount(2));
+console.log(oddCount(5));
